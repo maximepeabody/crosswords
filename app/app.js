3df3352c712f3b89c1079f05f6c33979aa7c256f
maxime peabody <maximepeabody@gmail.com>
2020-03-18 20:16:26 -0700

    test
    test

--- app/app.js
+++ app/app.js
@@ -1,7 +1,8 @@
-var app = angular.module('myApp', ["firebase"]);
+var app = angular.module('myApp', ["firebase", "ngRoute"]);
 
 
-app.controller('myCtrl', function($scope, $firebaseObject) {
+app.controller('myCtrl', function($scope, $firebaseObject, $location) {
+  //console.log($location.search());
   // Initialize the Firebase SDK
   var config = {
     apiKey: 'AIzaSyC1xwRC3Ww0rFGUcRhO_WWuckLPPzcuNB4',
@@ -12,15 +13,15 @@ app.controller('myCtrl', function($scope, $firebaseObject) {
   firebase.initializeApp(config);
   var db = firebase.database();
 
-  var ref = db.ref().child("puzzles").child("1").child("game-4");
+  // var ref = db.ref().child("puzzles").child("1").child("game-4");
 
   // var ref = firebase.database().ref().child("boards").child("game1");
-  // download the data into a local object
-  var syncObject = $firebaseObject(ref);
+  //   download the data into a local object
+  // var syncObject = $firebaseObject(ref);
 
   // synchronize the object with a three-way data binding
   // click on `index.html` above to see it used in the DOM!
-  syncObject.$bindTo($scope, "game");
+  //   syncObject.$bindTo($scope, "game");
   //var board_length = $scope.game.board.length;
   //$scope.cell_width = 100/board_length + '%';
 
@@ -38,13 +39,14 @@ app.controller('myCtrl', function($scope, $firebaseObject) {
         } else {
           console.log("got puzzle. Now loading it into game")
           // Generate game / generate gamecode.
-          $scope.gamecode =  Math.random().toString(36).substring(2, 15)
+          gamecode =  Math.random().toString(36).substring(2, 15)
             + Math.random().toString(36).substring(2, 15);
           // Create a new game ref and copy the puzzle data there.
-          db.ref().child("games").child($scope.gamecode).set(puzzle);
+          db.ref().child("games").child(gamecode).set(puzzle);
           var gameSyncObject =  $firebaseObject(
-            db.ref().child("games").child($scope.gamecode));
+            db.ref().child("games").child(gamecode));
           gameSyncObject.$bindTo($scope, "game");
+          $scope.gamecode = gamecode;
         }
       });
   };
